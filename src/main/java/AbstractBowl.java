405df8fd06c62b993cc0823cab91accbbfd110fc
elcode13 <elkhanjafarov07@gmail.com>
2021-04-01 15:08:38 +0400

    Level 1.Java.Lesson 7 HW

--- src/main/java/AbstractBowl.java
+++ src/main/java/AbstractBowl.java
@@ -1,8 +1,10 @@
 public abstract class AbstractBowl {
     protected int amount;
+
     public void put(int amount) {
         this.amount += amount;
     }
+
     public void decrease(int amount) {
         this.amount -= amount;
     }
